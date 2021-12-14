aca32a06405c57ee5306a613a51f4ab6e26ee7de
VladislavZhuravovich <92027313+VladislavZhuravovich@users.noreply.github.com>
2021-12-14 19:56:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+// zadanie1
+
+let str = 'Привет';
+console.log(typeof str);
+
+let num = 123;
+console.log(typeof num);
+
+let flag = true;
+console.log(typeof flag);
+
+let txt = 'true';
+console.log(typeof txt);
+
+// zadanie2
+
+let q = 5;
+let p = 3;
+
+let a1 = q + p;
+console.log(a1);
+
+let a2 = q - p;
+console.log(a2);
+
+let a3 = q * p;
+console.log(a3);
+
+let a4 = q / p;
+console.log(a4);
+
+let a5 = q ** p;
+console.log(a5);
+
+// zadanie3
+
+let a6 = q % p;
+console.log(a6);
+
+let a7 = p % q;
+console.log(a7);
+
+let a8 = q + '3';
+console.log(a8);
+
+let a9 = '5' - p;
+console.log(a9);
+
+let a10 = 75 + 'кг';
+console.log(a10);
+
+// zadanie4
+
+let height = 23 ;
+let width = 10 ;
+
+let s = height * width;
+console.log(s);
+
+// zadanie5
+
+let heightC = 10;
+let dC = 4;
+
+let rC = dC / 2 ;
+console.log(rC);
+
+let v = Math.PI * (dC / 2) ** 2 * heightC;
+console.log(v);
+
+// zadanie6
+
+let n = 3;
+let m = 4;
+
+let k =  Math.sqrt (n ** 2 + m ** 2);
+console.log(k);
+
+
+
+
